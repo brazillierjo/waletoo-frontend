a7d0caf4fea76dea2c66e8b3a38109097fad7c67
brazillierjo <j.brazillier@gmail.com>
2022-12-23 21:20:52 +0100

    create env variables + login api request

--- src/APIs/Logs.ts
+++ src/APIs/Logs.ts
@@ -1,7 +1,10 @@
 import { api } from "./GenericApi";
 
+const API_URL = import.meta.env.VITE_API_URL;
+console.log(API_URL);
+
 export class Logs {
     static login = (username: string, password: string) => {
-        return api.post(`${process.env.REACT_APP_API_URL}/api/login`, { username, password });
+        return api.post(`${API_URL}/user/login`, { username, password });
     };
 }
