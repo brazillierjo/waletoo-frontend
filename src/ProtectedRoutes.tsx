abe48c8341b5c9816989872c51aa988cfd547b6b
brazillierjo <j.brazillier@gmail.com>
2022-12-24 02:13:50 +0100

    store user data in store redux

--- src/ProtectedRoutes.tsx
+++ src/ProtectedRoutes.tsx
@@ -4,19 +4,28 @@ import { LocalStorage } from "./helpers/LocalStorage";
 import jwt from "jwt-decode";
 import { useAppDispatch } from "./helpers/hooks";
 import { storeUser } from "./Redux/LoginSlices/LoginSlice";
+import { api } from "./APIs/GenericApi";
 
 export default function ProtectedRoutes() {
     const dispatch = useAppDispatch();
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const token = LocalStorage.get("token");
+    const API_URL = import.meta.env.VITE_API_URL;
 
     useEffect(() => {
         if (token) {
             const decodedToken: { id: string; exp: number; iat: number } = jwt(token);
             const userId = decodedToken.id;
 
-            dispatch(storeUser(userId));
-            setIsLoggedIn(true);
+            api.get(`${API_URL}/user/get/${userId}`).then((res) => {
+                if (res.status === 200) {
+                    dispatch(storeUser(res.data));
+                    setIsLoggedIn(true);
+                } else {
+                    setIsLoggedIn(false);
+                    window.location.href = "/connexion";
+                }
+            });
         } else {
             setIsLoggedIn(false);
             window.location.href = "/connexion";
